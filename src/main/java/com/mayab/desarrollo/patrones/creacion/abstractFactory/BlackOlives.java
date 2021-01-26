4b328f6859c9c6b18fd9256c3bb0e825155ccb48
Patricia Ortegon <patricia.ortegon@gmail.com>
2021-01-26 15:44:19 -0600

    Adding abstract factory

---
+++ src/main/java/com/mayab/desarrollo/patrones/creacion/abstractFactory/BlackOlives.java
@@ -0,0 +1,8 @@
+package com.mayab.desarrollo.patrones.creacion.abstractFactory;
+
+public class BlackOlives implements Veggies {
+
+	public String toString() {
+		return "Black Olives";
+	}
+}
---
+++ src/main/java/com/mayab/desarrollo/patrones/creacion/abstractFactory/MarinaraSauce.java
@@ -0,0 +1,7 @@
+package com.mayab.desarrollo.patrones.creacion.abstractFactory;
+
+public class MarinaraSauce implements Sauce {
+	public String toString() {
+		return "Marinara Sauce";
+	}
+}
