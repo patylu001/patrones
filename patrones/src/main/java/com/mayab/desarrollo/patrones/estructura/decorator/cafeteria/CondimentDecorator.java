8fedbc81473be820baf93e225ae9712abaf73621
Patricia Ortegon <patricia.ortegon@gmail.com>
2021-02-02 19:08:47 -0600

    Agregando decorator

---
+++ patrones/src/main/java/com/mayab/desarrollo/patrones/estructura/decorator/cafeteria/CondimentDecorator.java
@@ -0,0 +1,5 @@
+package com.mayab.desarrollo.patrones.estructura.decorator.cafeteria;
+
+public abstract class CondimentDecorator extends Beverage {
+	public abstract String getDescription();
+}
---
+++ patrones/src/main/java/com/mayab/desarrollo/patrones/estructura/decorator/cafeteria/Mocha.java
@@ -0,0 +1,17 @@
+package com.mayab.desarrollo.patrones.estructura.decorator.cafeteria;
+
+public class Mocha extends CondimentDecorator {
+	Beverage beverage;
+ 
+	public Mocha(Beverage beverage) {
+		this.beverage = beverage;
+	}
+ 
+	public String getDescription() {
+		return beverage.getDescription() + ", Mocha";
+	}
+ 
+	public double cost() {
+		return .20 + beverage.cost();
+	}
+}
