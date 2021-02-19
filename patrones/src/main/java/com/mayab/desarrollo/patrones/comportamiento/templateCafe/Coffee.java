d2154f51990d8adb751741004eabd3c8d7308ea1
Patricia Ortegon <patricia.ortegon@gmail.com>
2021-02-18 19:45:11 -0600

    Agregando template

---
+++ patrones/src/main/java/com/mayab/desarrollo/patrones/comportamiento/templateCafe/Coffee.java
@@ -0,0 +1,10 @@
+package com.mayab.desarrollo.patrones.comportamiento.templateCafe;
+
+public class Coffee extends CaffeineBeverage {
+	public void brew() {
+		System.out.println("Dripping Coffee through filter");
+	}
+	public void addCondiments() {
+		System.out.println("Adding Sugar and Milk");
+	}
+}
---
+++ patrones/src/main/java/com/mayab/desarrollo/patrones/comportamiento/templateCafe/Tea.java
@@ -0,0 +1,10 @@
+package com.mayab.desarrollo.patrones.comportamiento.templateCafe;
+
+public class Tea extends CaffeineBeverage {
+	public void brew() {
+		System.out.println("Steeping the tea");
+	}
+	public void addCondiments() {
+		System.out.println("Adding Lemon");
+	}
+}
